d6a51fe4fa62aeaa8155aa4190f6b2cdea6548df
Ragnor16 <84565998+Ragnor16@users.noreply.github.com>
2023-02-20 21:05:13 +0530

    Add files via upload

---
+++ HomeAnim.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './HomeAnim.css'
+
+
+
+function HomeAnim() {
+  
+  
+  return (
+
+	<div>
+<div class="loop-wrapper">
+  <div class="mountain"></div>
+  <div class="hill"></div>
+  <div class="tree"></div>
+  <div class="tree"></div>
+  <div class="tree"></div>
+  <div class="rock"></div>
+  <div class="truck"></div>
+  <div class="wheels"></div>
+</div> 
+</div>
+  )
+}
+
+export default HomeAnim
